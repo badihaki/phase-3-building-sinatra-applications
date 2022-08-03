359292a6ad4a28095caed7a1359970a483034fd5
Badi-Haki Martin <jojackblack@gmail.com>
2022-08-03 10:40:27 -0400

    assignment completed

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,11 @@
+class ApplicationController < Sinatra::Base
+
+    get '/' do
+      '
+      <h1>Hello <em>World</em>!</h1>
+      <h2>Haki''s edit ri''chere</h2>
+      <text>how to make the apostraphe???</text>
+      '
+    end
+  
+  end
